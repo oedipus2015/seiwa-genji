525461fea8bb0a5baa8040e79beecbfa75aadd86
oedipus2015 <niitaka12082400@yahoo.co.jp>
2026-03-22 15:57:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,11 @@ document.addEventListener("DOMContentLoaded", () => {
             // ★ ノード側の desc を textarea に変える
             OrgChart.templates.olivia.field_2 =
                 '<textarea class="oc-desc" style="width:100%;height:auto;white-space:pre-wrap;word-break:break-word;border:none;background:transparent;resize:none;" readonly>{val}</textarea>';
-
+            
+            // ★ ここに追加！！（\n → <br> に変換）
+            OrgChart.templates.olivia.field_2 =
+                OrgChart.templates.olivia.field_2.replace("{val}", "{val}".replace(/\n/g, "<br>"));
+            
             chart.load(nodes);
 
         })
