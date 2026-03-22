948cf321a9b8c46eeea9ecbe391e261303795822
oedipus2015 <niitaka12082400@yahoo.co.jp>
2026-03-22 20:34:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,34 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
                     field_0: "name",
                     field_1: "title",
                     field_2: "desc",
-                    img_0: "img",
-                    wiki: "wiki"
-                },
-
-                // ★ 標準ポップアップを開く
-                nodeClick: OrgChart.action.edit,
-
-                editForm: {
-                    readOnly: true,
-                    photoBinding: "img",
-
-                    buttons: {
-                        wiki: {
-                            icon: OrgChart.icon.link,
-                            text: "Wikipedia",
-                            onClick: function (nodeId) {
-                                const node = chart.get(nodeId);
-                                if (node.wiki) {
-                                    window.open(node.wiki, "_blank");
-                                }
-                            }
-                        }
-                    }
+                    img_0: "img"
                 }
             });
-
-            // ★ ここは削除！ textarea に変えると壊れる
-            // OrgChart.templates.olivia.field_2 = ...
 
             chart.load(nodes);
 
