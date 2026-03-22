9d2d6dbe1bfb104947380c6a6de0fd779f410ffd
oedipus2015 <niitaka12082400@yahoo.co.jp>
2026-03-22 17:35:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         }
                     }
                 }
-            }
+            },
 
                 nodeClick: (node) => {
                     showPanel(node);
